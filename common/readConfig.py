6dc674734956721001ee2c1687806b0d4df12937
NJ-zero <821854810@qq.com>
2018-04-10 20:20:21 +0800

    config.ini path优化

--- common/readConfig.py
+++ common/readConfig.py
@@ -6,8 +6,10 @@ import configparser
 import codecs
 
 # 获取本文件所在路径
-prjDir = os.path.split(os.path.realpath(__file__))[0]
-configfile_path = os.path.join(prjDir, "config.ini")
+# prjDir = os.path.split(os.path.realpath(__file__))[0]
+# configfile_path = os.path.join(prjDir, "config.ini")
+
+configfile_path = './config.ini'
 
 class ReadConfig:
     def __init__(self):
@@ -25,3 +27,6 @@ class ReadConfig:
         value = self.conf.get('loginconf',name)
         return value
 
+# 测试代码
+# a = ReadConfig()
+# print  a.getdbConfigValue('host')
