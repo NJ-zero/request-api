4eb5f500c4708348333a075830548f80a24f8ae4
NJ-Zero <821854810@qq.com>
2018-05-11 12:55:41 +0800

    支持串接口，增加列表存储jsonpath解析的结果

--- testcase/test_cm.py
+++ testcase/test_cm.py
@@ -20,13 +20,17 @@ class Cm(unittest.TestCase):
     def test_cm(self):
         cookie = test_login.login()
         common_url=conf.ReadConfig().getloginConfigValue('url')
-
+        jsondatas=['data']
         for i in range(int(get.get_nrows('cm'))-1):
+            #将jsonpath存入列表中
+            jsondata=get.get_jsonpath('cm',i+1)
+            if jsondata !='':
+                jsondatas.append(jsondata)
             # 判断是否执行
             if int(get.get_data('cm',i+1,4))== 1:
                 login_url=get.get_data('cm',i+1,2)
                 url = common_url + login_url
-                data = get.get_formdata('cm',i+1)
+                data = get.get_formdata('cm',i+1,jsondatas[-1])
                 header = {"Content-Type": "application/x-www-form-urlencoded","Cookie":cookie}
                 global r
                 print url
