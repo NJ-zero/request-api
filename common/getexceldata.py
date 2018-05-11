62f11ac309caae0f072e5887a6551cfe14a30f86
NJ-Zero <821854810@qq.com>
2018-05-11 12:50:20 +0800

    增加读取依赖那一列的方法，优化formdata读取，增肌正则匹配与value替换

--- common/getexceldata.py
+++ common/getexceldata.py
@@ -3,7 +3,7 @@
 
 import xlrd,json,xlwt
 import requests
-import os
+import os,re
 
 # file = r'E:\Request-Excel\testdata\test_data.xls'
 file = '../testdata/test_data.xls'
@@ -22,7 +22,7 @@ def get_data(name,rows,cols):
     value = sheet.cell(rows,cols).value
     return value
 
-def get_formdata(name,rows):
+def get_formdata(name,rows,change):
     '''
     读取请求参数
     :param name: sheet名称
@@ -36,7 +36,14 @@ def get_formdata(name,rows):
     if value == '':
         return value
     else:
-        return json.loads(value)
+        value=json.loads(value)
+        for k in value.keys():
+            pattern = re.search(r"^change{change}", str(value[k]))
+            if pattern != None:
+                value[k] = change
+            elif pattern is None:
+                pass
+        return value
 
 def get_nrows(name):
     '''
@@ -49,6 +56,29 @@ def get_nrows(name):
     nrows = sheet.nrows
     return nrows
 
+def get_jsonpath(name,rows):
+    '''
+    读取依赖，供下面参数传值
+    :param name: sheet名称
+    :param rows: 行
+    :return:
+    '''
+    alldata = xlrd.open_workbook(file)
+    sheet = alldata.sheet_by_name(name)
+
+    value = sheet.cell(rows, 15).value
+    if value == '' or value is None:
+        return ''
+    else:
+        return str(value)
+
 #  测试
-# a = get_data('cm',2,3)
-# print a
+# a = get_jsonpath('cm',1)
+# print a,type(a)
+# from jsonpath import jsonpath
+# dict = {"a":"aaa","class": {"students": [{"student_id": "1", "name": "bob", "sex": "male", "age": 6},
+#                                {"student_id": "2", "name": "amy", "sex": "female", "age": 6},
+#                                {"student_id": "3", "name": "pery", "sex": "male", "age": 5}],
+#                   "teachers": {"teacher_id": "1", "name": "anne", "sex": "female", "age": 32}}}
+# name =jsonpath(dict, a)
+# print(name)
